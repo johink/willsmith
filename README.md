b81a410244a2a57ece2fc68f0de08aae62eb5144
Chad Reynolds <cjreynol13@aol.com>
2018-03-27 23:49:41 -0500

    Add ArgumentParser and update the README
    Run python main.py -h to see available options.  Currently the
    parser hardcodes expecting two agents, but this can probably be
    replaced in the future using a single argment and nargs = '*'.
    That would require some additional checking to ensure the number
    of agents matches the expected number of agents for a game.

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Monte Carlo Tree Search
 
-An implementation and testing of the MCTS algorithm to improve my 
-understanding of how it works and how successful. 
+An implementation of the MCTS algorithm as an adversarial game playing agent.  
 
-Work is being done along with [John Bourassa](https://github.com/johink).
+More details about the algorithm can be found on Wikipedia 
+[here](https://en.wikipedia.org/wiki/Monte_Carlo_tree_search).  
+
+Work is being done along with [John Bourassa](https://github.com/johink).  
+
+## Included
+
+The repository currently has an implementation of Nested Tic-Tac-Toe.  This 
+plays similarly to the standard game, except instead of 9 spaces there are 
+9 inner Tic-Tac-Toe boards to play.  Players can choose any legal move on any 
+inner board, and the winner of an inner board claims that space on the outer 
+board.  
+
+> Havannah implementation coming soon.
+
+Included agent types:  
+- MCTSAgent:  runs the aforementioned algorithm for action selection  
+- RandomAgent: randomly chooses an action from the action space on each turn  
+- HumanAgent: prompts user for action to take  
+
+## To Run
+
+Run `python main.py -h` to see the currently available options.  
--- main.py
+++ main.py
@@ -1,9 +1,53 @@
+from argparse import ArgumentParser
+
 from agents.human_agent import HumanAgent
 from agents.mcts_agent import MCTSAgent
 from agents.random_agent import RandomAgent
 from games.ttt.nested_ttt import NestedTTT
 from willsmith.simulator import Simulator
 
+
+def create_parser():
+    parser = ArgumentParser(description = "Run agents through simulations")
+
+    parser.add_argument("game_choice", type = str, 
+                        choices = ["NestedTTT", "ttt"],
+                        help = "The game for the agents to play")
+    parser.add_argument("-a", "--agent1", type = str, default = "mcts",
+                        choices = ["mcts", "rand", "human"],
+                        help = "Agent type for player 1")
+    parser.add_argument("-b", "--agent2", type = str, default = "rand",
+                        choices = ["mcts", "rand", "human"],
+                        help = "Agent type for player 2")
+    parser.add_argument("-t", "--time_allotted", type = float, default = 1.0,
+                        help = "Time allotted for agent moves")
+    return parser
+
+def lookup_agent(agent_str):
+    lookup = {"mcts" : MCTSAgent, "rand" : RandomAgent, "human" : HumanAgent}
+    try:
+        agent = lookup[agent_str]
+    except KeyError:
+        agent = None
+
+    return agent
+
 if __name__ == "__main__":
-    simulator = Simulator(NestedTTT, [MCTSAgent, RandomAgent], 0.5)
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.game_choice == "NestedTTT" or args.game_choice == "ttt":
+        game = NestedTTT
+    else:
+        raise RuntimeError("Unexpected game type.")
+
+    agent1 = lookup_agent(args.agent1)
+    agent2 = lookup_agent(args.agent2)
+
+    if agent1 is None or agent2 is None:
+        raise RuntimeError("Unexpected agent type.")
+
+    time = args.time_allotted
+
+    simulator = Simulator(game, [agent1, agent2], time)
     simulator.run_game()
