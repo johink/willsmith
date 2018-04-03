dfc440a93af34374251ec31f710085f45a6b9d0e
Chad Reynolds <cjreynol13@aol.com>
2018-04-03 14:24:56 -0500

    Add test that Havannah game not affected by copy

--- tests/games/havannah/test_havannah.py
+++ tests/games/havannah/test_havannah.py
@@ -24,3 +24,9 @@ class TestHavannah(TestCase):
         other_game = Havannah(2)
         other_game.take_action_if_legal(action)
         self.assertEqual(self.game, other_game)
+
+    def test_deepcopy_actions_do_not_affect_orig(self):
+        other_game = self.game.copy()
+        action = HavannahAction((0, 0, 0), Color.BLUE)
+        other_game.take_action_if_legal(action)
+        self.assertNotEqual(self.game, other_game)
