26ea63304089a2192f793850a31203ae89fab8bd
Chad Reynolds <cjreynol13@aol.com>
2018-05-02 22:33:01 -0500

    Change gridworld terminal squares coloring method
    Now, instead of all positive rewards being one color and all negative
    rewards being another the colors are scaled based on the rewards value
    in comparison to the others.
    
    Rewards closer to zero are darker, while more positive and negative
    rewards are brighter green and red respectively.

--- games/gridworld/gridworld_display.py
+++ games/gridworld/gridworld_display.py
@@ -25,13 +25,11 @@ class GridworldDisplay(GUIDisplayController):
     SQUARE_BORDER_COLOR = "#000000"
     SQUARE_AGENT_COLOR = "#FFFF00"
 
-    TERM_COLOR_POS = "#00FF00"
-    TERM_COLOR_NEG = "#FF0000"
-
     def __init__(self):
         super().__init__()
         self.canvas = None
         self.height = None
+        self.terminal_colors = None
 
     def _initialize_widgets(self):
         self.canvas = Canvas(self.root)
@@ -61,8 +59,10 @@ class GridworldDisplay(GUIDisplayController):
         self.height = (height * self.SQUARE_WIDTH 
                         + self.SQUARE_BORDER * 4)
         self.root.geometry("{}x{}".format((width * self.SQUARE_WIDTH 
-                                                + self.SQUARE_BORDER * 4), 
+                                            + self.SQUARE_BORDER * 4), 
                                             self.height))
+
+        self.lookup = self._create_color_lookup(state)
 
         for pos in state.grid.keys():
             agent_square = False
@@ -70,6 +70,53 @@ class GridworldDisplay(GUIDisplayController):
                 agent_square = True
             canvas_pos = self._square_coord_to_canvas_coord(*pos)
             self._draw_square(state.grid[pos], canvas_pos, agent_square)
+
+    def _create_color_lookup(self, state):
+        """
+        Create a dictionary from terminal reward values to a hex color string.
+
+        0 is black, negative rewards are shades of red, positive rewards are 
+        shades of green.
+        """
+        neg_rewards, pos_rewards = self._get_terminal_rewards_lists(state)
+        neg_color_step = (256 - len(neg_rewards)) // len(neg_rewards)
+        pos_color_step = (256 - len(pos_rewards)) // len(pos_rewards)
+
+        lookup = {0 : "#000000"}
+        lookup.update({reward : self._num_to_hex_string((i + 1) * neg_color_step, True)
+                    for i, reward in enumerate(neg_rewards)})
+        lookup.update({reward : self._num_to_hex_string((i + 1) * pos_color_step, False)
+                        for i, reward in enumerate(pos_rewards)})
+        return lookup
+
+    def _num_to_hex_string(self, num, is_red):
+        """
+        Return an RGB hex string with the hex value of the given num.
+
+        is_red determines which position the hex value from num is used in, 
+        with the other two colors being 00.
+        """
+        hex_str = hex(num)[2:]      # remove 0x from the front of the str
+        if is_red:
+            color = "#{}0000".format(hex_str)
+        else:
+            color = "#00{}00".format(hex_str)
+        return color
+
+    def _get_terminal_rewards_lists(self, state):
+        """
+        Return two lists, one each for the negative and positive terminal 
+        rewards.
+        """
+        neg_rewards = []
+        pos_rewards = []
+        for square in state.grid.values():
+            if square.terminal:
+                if square.reward < 0 and square.reward not in neg_rewards:
+                    neg_rewards.append(square.reward)
+                elif square.reward > 0 and square.reward not in pos_rewards:
+                    pos_rewards.append(square.reward)
+        return sorted(neg_rewards), sorted(pos_rewards)
 
     def _square_coord_to_canvas_coord(self, x, y):
         """
@@ -88,7 +135,7 @@ class GridworldDisplay(GUIDisplayController):
         """
         Draw a colored square at the given location.
 
-        If the agent is there, draw a smaller box indicating this.
+        If the agent is in that location, draw a smaller box to show it.
         """
         square_color = self._determine_square_color(square)
         self.canvas.create_rectangle(pos[0], pos[1], 
@@ -112,8 +159,6 @@ class GridworldDisplay(GUIDisplayController):
         terminal and its reward.
         """
         color = self.SQUARE_EMPTY_COLOR
-        if square.terminal and square.reward < 0:
-            color = self.TERM_COLOR_NEG
-        elif square.terminal and square.reward > 0:
-            color = self.TERM_COLOR_POS
+        if square.terminal:
+            color = self.lookup[square.reward]
         return color
